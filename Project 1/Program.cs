7fe36da8a9aa488309b2e2726967494c41d742f1
jarrett leland <JarrettLeland@macallister.com>
2022-07-22 08:00:34 -0400

    new project files

---
+++ Project 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace FileUploader
+{
+    public class Program
+    {
+        public string _connectionString = "DefaultEndpointsProtocol=https;AccountName=custimage;AccountKey=YmP2PEIgU+4g9N/fFxOgiF7AqdfnjkrkrYINKj4WkgIEOnhrDamCFWnw6/uWz0gd0zAcwB/jgIhS+AStl2sNgA==;EndpointSuffix=core.windows.net";
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- mvc/BlobStorage/BlobStorage/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace BlobStorage.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
